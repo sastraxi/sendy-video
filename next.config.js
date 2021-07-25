1f6519977b198363ed3eccbf343924ce878e622a
Cameron Gorrie <sastraxi@gmail.com>
2021-07-25 14:24:45 -0400

    _http_common fix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, {isServer}) => {
+    if (isServer) {
+      // https://github.com/prisma/prisma/issues/6564 
+      // remove when prisma pushes a fix w/undici@4
+      config.externals.push('_http_common');
+    }
+    return config;
+  },
+  target: 'serverless',
 }
