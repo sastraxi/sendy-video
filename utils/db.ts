bce33de2b0295e105aeec66e393793e2c7ca147e
Cameron Gorrie <sastraxi@gmail.com>
2021-07-28 14:53:17 -0400

    log db queries

--- utils/db.ts
+++ utils/db.ts
@@ -1,4 +1,39 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
-export default prisma
+// Prevent multiple instances of Prisma Client in development
+declare const global: NodeJS.Global & { prisma?: PrismaClient };
+
+let prisma: PrismaClient;
+if (!global.prisma) {
+  prisma = new PrismaClient({
+    log: [
+      {
+        emit: "event",
+        level: "query",
+      },
+      {
+        emit: "stdout",
+        level: "error",
+      },
+      {
+        emit: "stdout",
+        level: "info",
+      },
+      {
+        emit: "stdout",
+        level: "warn",
+      },
+    ],
+  });
+
+  prisma.$on("query", (e) => {
+    console.log("Query: " + e.query);
+    console.log("Duration: " + e.duration + "ms");
+  });
+
+  if (process.env.NODE_ENV === "development") global.prisma = prisma;
+} else {
+  prisma = global.prisma;
+}
+
+export default prisma;
