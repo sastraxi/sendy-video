74603345845c946be7a98236db21d6b16771a4fa
Cameron Gorrie <sastraxi@gmail.com>
2021-07-29 16:04:16 -0400

    add logrocket

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,16 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'next-auth/client'
 import { ChakraProvider } from "@chakra-ui/react"
 
+import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
+
+// only initialize when in the browser
+if (typeof window !== 'undefined') {
+  LogRocket.init('a1zphg/sendy-video');
+  // plugins should also only be initialized when in the browser
+  setupLogRocketReact(LogRocket);
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session} >
