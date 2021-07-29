5c0201588b046169bc1dc78d30d4bf583adc3992
Cameron Gorrie <sastraxi@gmail.com>
2021-07-28 23:09:40 -0400

    initial ordering

---
+++ components/TopMenu.tsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+import { 
+
+const FancyVideo = styled.video<{ mirror?: boolean }>`
+  border-radius: 4px;
+  transform: scaleX(${props => props.mirror ? -1  : 1});
+`;
+
+type VideoProps = VideoHTMLAttributes<HTMLVideoElement> & {
+  srcObject?: MediaStream;
+  mirror?: boolean;
+};
+
+const Video = ({ srcObject, ...props }: VideoProps) => {
+  const refVideo = useCallback(
+    (node: HTMLVideoElement) => {
+      if (node && srcObject) node.srcObject = srcObject;
+    },
+    [srcObject],
+  );
+
+  return <FancyVideo ref={refVideo} {...props} />;
+};
+
+export default Video;
